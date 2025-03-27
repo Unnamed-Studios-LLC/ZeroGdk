78c165a2be6ee8eba18f65e1fd17417fb8585e9b
Tucker Ulrich <5263190+Juiix@users.noreply.github.com>
2025-03-27 13:35:40 -0600

    World not null on WorldSystems

--- src/ZeroGdk.Server/Engine/WorldSystem.cs
+++ src/ZeroGdk.Server/Engine/WorldSystem.cs
@@ -1,4 +1,6 @@
-﻿namespace ZeroGdk.Server
+﻿using Arch.Core;
+
+namespace ZeroGdk.Server
 {
 	/// <summary>
 	/// Represents a base class for a game or simulation system that operates within a <see cref="World"/> context.
@@ -9,7 +11,11 @@
 		/// <summary>
 		/// The world this system is currently associated with. Set when the system is added to a world.
 		/// </summary>
-		public World? World { get; internal set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+		public World World { get; internal set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+		public Entities Entities => World.Entities;
 
 		/// <summary>
 		/// Called once when the system is first started. Override to perform initialization logic.
